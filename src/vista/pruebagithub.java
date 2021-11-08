51bb0664bcf4b89e72f785a186c9e5cda53f569e
judit2609 <muequitadetodosjugu@idea-PC>
2021-11-08 01:26:07 -0600

    prueba 1

---
+++ src/vista/pruebagithub.java
@@ -0,0 +1,13 @@
+
+package vista;
+
+import javax.swing.JOptionPane;
+
+public class pruebagithub {
+    
+    
+    public static void main(String[] args){
+        JOptionPane.showMessageDialog(null,"Prueba Git Hub\nGallegos\nIngenieria de software");
+        System.err.println("Hola");
+}
+}
